a12089109db008122a907134f07ab1e6a2e7120d
Katamaranas <ernestas.zidokas@gmail.com>
2019-04-16 11:32:04 +0300

    connection class hotfix

--- core/classes/Database/Connection.php
+++ core/classes/Database/Connection.php
@@ -10,18 +10,30 @@ class Connection extends \Core\Database\Abstracts\Connection {
         $this->setCredentials($creds);
     }
 
+    /**
+     * Connect To Database
+     * #1 Reikia patikrinti ar $this->pdo nera null (jeigu null, vadinasi jau priconnectinom)
+     * #2 Jei ne, sukurti $this->pdo = new PDO ...
+     * #3 Jeigu globaline konstanta DEBUG yra nustatyta, executinti šias dvi eilutes
+     *  $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+     *  $this->pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, true);
+     * Jeigu nepavyko sukurti PDO instancijos, throw'inti exceptioną panaudojant try, catch
+     * @throws Exception
+     */
     public function connect() {
-        try {
-            $this->pdo = new \PDO
-                    ("mysql:host=$this->host", $this->user, $this->pass);
-            if (DEBUG) {
-                $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXEPTION
-                );
-                $this->pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, true
-                );
+        if (!$this->pdo) {
+            try {
+                $this->pdo = new \PDO
+                        ("mysql:host=$this->host", $this->user, $this->pass);
+                if (DEBUG) {
+                    $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXEPTION
+                    );
+                    $this->pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, true
+                    );
+                }
+            } catch (\PDOException $e) {
+                throw new \PDOException('Could not connect to the database');
             }
-        } catch (\PDOException $e) {
-            throw new \PDOException('Could not connect to the database');
         }
     }
 
